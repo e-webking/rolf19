d7eeed71c67162e3e897d524cf81c82b15ae115c
Anisur <anisur.mullick@gmail.com>
2019-10-16 13:25:23 +0530

    [F] 8 images now linked

--- typo3conf/ext/armtpl/ext_emconf.php
+++ typo3conf/ext/armtpl/ext_emconf.php
@@ -26,7 +26,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '8.2.2',
+	'version' => '8.2.3',
 	'constraints' =>
 	array (
 		'depends' =>
